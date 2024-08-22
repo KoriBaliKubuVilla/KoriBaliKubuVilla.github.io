0012a5113e0a6e9e4c6e3f8c4ffef7d22c12bf21
KoriBaliKubuVilla <Koribalikubuvilla@gmail.com>
2024-08-23 04:17:14 +0800

    test overlay 5

--- js/main.js
+++ js/main.js
@@ -5,16 +5,40 @@
 
 (function($) {
 
+	"use strict";
 
+	var isMobile = {
+		Android: function() {
+			return navigator.userAgent.match(/Android/i);
+		},
+			BlackBerry: function() {
+			return navigator.userAgent.match(/BlackBerry/i);
+		},
+			iOS: function() {
+			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+		},
+			Opera: function() {
+			return navigator.userAgent.match(/Opera Mini/i);
+		},
+			Windows: function() {
+			return navigator.userAgent.match(/IEMobile/i);
+		},
+			any: function() {
+			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+		}
+	};
 
-	$(window).stellar({
-    responsive: true,
-    parallaxBackgrounds: true,
-    parallaxElements: true,
-    horizontalScrolling: false,
-    hideDistantElements: false,
-    scrollProperty: 'scroll'
-  });
+	if (!isMobile.any()) {
+		$(window).stellar({
+			responsive: true,
+			parallaxBackgrounds: true,
+			parallaxElements: true,
+			horizontalScrolling: false,
+			hideDistantElements: false,
+			scrollProperty: 'scroll'
+		  });
+	}
+	
   
 
 
@@ -39,7 +63,8 @@
 	};
 	loader();
 
-
+	// Scrollax
+   $.Scrollax();
 
 	var carousel = function() {
 		$('.home-slider').owlCarousel({
